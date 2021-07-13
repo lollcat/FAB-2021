fd9f2271eda933f09b03157b2ea8329011fde4f9
Laurence Midgley <loll.midgley@gmail.com>
2021-07-13 14:25:59 +0100

    decrease batch size for memory

--- AIS_train/Problems/18Well_mix.py
+++ AIS_train/Problems/18Well_mix.py
@@ -1,5 +1,8 @@
 if __name__ == '__main__':
     epochs = 12000
+    n_flow_steps = 30
+    n_distributions = 20
+    batch_size = int(1e3)
     import pathlib
     import os
     import sys
@@ -28,20 +31,21 @@ if __name__ == '__main__':
         plot_samples_vs_contours_many_well(*args, **kwargs)
 
 
-    save_path = pathlib.Path("16_dim_ReverseIAF_mix")
+    save_path = pathlib.Path(f"16_dim_ReverseIAF_mix_epochs{epochs}_flowsteps{n_flow_steps}_dist{n_distributions}")
     save_path.mkdir(parents=True, exist_ok=False)
     dim = 16
     target = ManyWellEnergy(dim=dim, a=-0.5, b=-6)
     n_samples_expectation = int(1e5)
     n_samples = int(1e4)
 
-    learnt_sampler = FlowModel(x_dim=dim, scaling_factor=2.0, flow_type="ReverseIAF_MIX", n_flow_steps=20,
+    learnt_sampler = FlowModel(x_dim=dim, scaling_factor=2.0, flow_type="ReverseIAF_MIX", n_flow_steps=n_flow_steps,
                                nodes_per_x=1.5)
-    tester = AIS_trainer(target, learnt_sampler, loss_type=False, n_distributions=20, n_steps_transition_operator=1,
+    tester = AIS_trainer(target, learnt_sampler, loss_type=False, n_distributions=n_distributions,
+                         n_steps_transition_operator=1,
                          step_size=1.0, transition_operator="HMC", learnt_dist_kwargs={"lr": 1e-4},
                          loss_type_2="alpha_2", train_AIS_params=True, inner_loop_steps=5)
 
-    history = tester.train(epochs, batch_size=int(1e3), intermediate_plots=True, n_plots=20, plotting_func=plotter)
+    history = tester.train(epochs, batch_size=batch_size, intermediate_plots=True, n_plots=20, plotting_func=plotter)
     plot_history(history)
     multipage(str(save_path / "training.pdf"))
     plt.close("all")
