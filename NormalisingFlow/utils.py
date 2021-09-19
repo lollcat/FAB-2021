ea4dd6adddea7a39cc87a88557e3ae8456b87654
Laurence Midgley <loll.midgley@gmail.com>
2021-09-20 00:07:30 +0100

    add HMC hooks and print

--- NormalisingFlow/utils.py
+++ NormalisingFlow/utils.py
@@ -5,10 +5,12 @@ class Monitor_NaN:
     def __init__(self, name=None):
         self.found_Nan = False
 
-    def overwrite_NaN_grad(self, grad, name=None, print_=True, replace_with=0.0):
+    def overwrite_NaN_grad(self, grad, name=None, print_=True, replace_with=0.0,
+                           print_first_time_only=False):
         if True in torch.isnan(grad):
             if self.found_Nan is False and print_:
                 print(f"found a NaN and overwrote it during flow gradient calculation: {name}")
-                self.found_Nan = True
+                if print_first_time_only:
+                    self.found_Nan = True
             grad[torch.isnan(grad) | torch.isinf(grad)] = replace_with
         return grad
